c5f478498be31cdcfbc9d0986623a3a0c3283b55
PK4US <Stepanchuk.ei@gmail.com>
2023-02-03 00:17:16 +0200

    Initial commit

---
+++ app/src/main/java/com/pk4us/jetpackcompose_componentsexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.pk4us.jetpackcompose_componentsexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.pk4us.jetpackcompose_componentsexample.ui.theme.Material3AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           Material3AppTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
