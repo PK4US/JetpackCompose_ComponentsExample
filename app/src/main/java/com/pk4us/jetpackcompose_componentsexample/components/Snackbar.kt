445964103ea9a0a3e18e7419030ed341e513b80c
PK4US <stepanchuk.ei@gmail.com>
2023-04-01 19:52:13 +0300

    Snackbar

---
+++ app/src/main/java/com/pk4us/jetpackcompose_componentsexample/components/Snackbar.kt
@@ -0,0 +1,145 @@
+package com.pk4us.jetpackcompose_componentsexample.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun MySnackbarScreen() {
+    Snackbar()
+//    Snackbar2()
+}
+
+@Composable
+@Preview
+@ExperimentalMaterial3Api
+fun Snackbar() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        floatingActionButton = {
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                var clickCount by remember { mutableStateOf(0) }
+
+                ExtendedFloatingActionButton(
+                    onClick = {
+                        // show snackbar as a suspend function
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                "Snackbar 1 # ${++clickCount}"
+                            )
+                        }
+                    }
+                ) { Text("snackbar 1") }
+
+                ExtendedFloatingActionButton(
+                    onClick = {
+                        // show snackbar as a suspend function
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = "Snackbar 2 # ${++clickCount}",
+                                actionLabel = "Action",
+                                withDismissAction = true,
+                                duration = SnackbarDuration.Indefinite
+                            )
+                        }
+                    }
+                ) { Text("snackbar 2") }
+            }
+        },
+        content = { innerPadding ->
+            Text(
+                text = "Body content",
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .wrapContentSize()
+            )
+        }
+    )
+}
+
+
+class SnackbarVisualsWithError(override val message: String, val isError: Boolean) :
+    SnackbarVisuals {
+    override val actionLabel: String
+        get() = if (isError) "Error" else "OK"
+    override val withDismissAction: Boolean
+        get() = false
+    override val duration: SnackbarDuration
+        get() = SnackbarDuration.Indefinite
+}
+
+@Composable
+@Preview
+@ExperimentalMaterial3Api
+fun Snackbar2() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(snackbarHostState) { data ->
+                val isError = (data.visuals as? SnackbarVisualsWithError)?.isError ?: false
+                val buttonColor = if (isError) {
+                    ButtonDefaults.textButtonColors(
+                        containerColor = MaterialTheme.colorScheme.errorContainer,
+                        contentColor = MaterialTheme.colorScheme.error
+                    )
+                } else {
+                    ButtonDefaults.textButtonColors(
+                        contentColor = MaterialTheme.colorScheme.inversePrimary
+                    )
+                }
+
+                Snackbar(
+                    modifier = Modifier
+                        .border(2.dp, MaterialTheme.colorScheme.secondary)
+                        .padding(12.dp),
+                    action = {
+                        TextButton(
+                            onClick = { if (isError) data.dismiss() else data.performAction() },
+                            colors = buttonColor
+                        ) { Text(data.visuals.actionLabel ?: "") }
+                    }
+                ) {
+                    Text(data.visuals.message)
+                }
+            }
+        },
+        floatingActionButton = {
+            var clickCount by remember { mutableStateOf(0) }
+            ExtendedFloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            SnackbarVisualsWithError(
+                                "Snackbar # ${++clickCount}",
+                                isError = clickCount % 2 != 0
+                            )
+                        )
+                    }
+                }
+            ) { Text("Show snackbar") }
+        },
+        content = { innerPadding ->
+            Text(
+                text = "Custom Snackbar Demo",
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .wrapContentSize()
+            )
+        }
+    )
+}
