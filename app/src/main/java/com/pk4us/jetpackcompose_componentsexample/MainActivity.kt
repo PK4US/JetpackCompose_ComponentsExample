739ae71ab660eba02276ea18114f25549ac07275
PK4US <stepanchuk.ei@gmail,com>
2023-04-16 22:58:47 +0300

    StatusBar And NavigationBar

--- app/src/main/java/com/pk4us/jetpackcompose_componentsexample/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcompose_componentsexample/MainActivity.kt
@@ -3,23 +3,118 @@ package com.pk4us.jetpackcompose_componentsexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.pk4us.jetpackcompose_componentsexample.components.MyCalendarScreen
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.core.view.WindowCompat
 import com.pk4us.jetpackcompose_componentsexample.ui.theme.Material3AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // расширил пользовательский интерфейс приложения до системных панелей
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
+
+        // вместо WindowCompat.setDecorFitsSystemWindows() вы также можете использовать следующее
+        // window.setFlags(
+        // WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        // WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        // )
+
         setContent {
             Material3AppTheme() {
+
+//                //__________________________________________________________________________________ цвет статус бара
+//                val systemUiController = rememberSystemUiController()
+//                SideEffect {
+//                    systemUiController.setStatusBarColor(
+//                        color = Color.Magenta,
+//                        darkIcons = false
+//                    )
+//                }//__________________________________________________________________________________
+//
+//
+//
+//                //__________________________________________________________________________________ цвет меню навигации
+//                val systemUiController = rememberSystemUiController()
+//                SideEffect {
+//                    systemUiController.setNavigationBarColor(
+//                        color = Color.Magenta,
+//                        darkIcons = false
+//                    )
+//                }//__________________________________________________________________________________
+//
+//
+//
+//                //__________________________________________________________________________________ цвет всех элементов управления
+//                val systemUiController = rememberSystemUiController()
+//                SideEffect {
+//                    systemUiController.setSystemBarsColor(
+//                        color = Color.Magenta,
+//                        darkIcons = false
+//                    )
+//                }//__________________________________________________________________________________
+//
+//
+//
+//                //__________________________________________________________________________________ скрывает элементы управления
+//                val systemUiController = rememberSystemUiController()
+//                SideEffect {
+//                    // предназначены для настройки видимости панели навигации
+//                    systemUiController.isNavigationBarVisible = false
+//
+//                    // предназначены для настройки видимости строки состояния соответственно
+//                    systemUiController.isStatusBarVisible = false
+//
+//                    // isSystemBarsVisible для обеих системных панелей.
+//                    //systemUiController.isSystemBarsVisible = false
+//                }//__________________________________________________________________________________
+//
+//
+//
+//                //__________________________________________________________________________________ делает прозрачным
+//                val systemUiController = rememberSystemUiController()
+//                SideEffect {
+//                    // установить прозрачный цвет, чтобы наше изображение было видно за строкой состояния
+//                    systemUiController.setStatusBarColor(color = Color.Transparent)
+//
+//                    // панель навигации прозрачная
+//                     systemUiController.setNavigationBarColor(color = Color.Transparent)
+//                }//__________________________________________________________________________________
+
+
+
+
+
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyCalendarScreen()
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+//                            .navigationBarsPadding() //добавляет отступ в меню навигазии
+//                            .statusBarsPadding()  //добавляет отступ в статус баре
+                    ) {
+                        items(count = 10) {
+                            Image(
+                                painter = painterResource(id = R.drawable.dog),
+                                contentDescription = "Squirrel",
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/pk4us/jetpackcompose_componentsexample/ui/theme/Theme.kt
+++ app/src/main/java/com/pk4us/jetpackcompose_componentsexample/ui/theme/Theme.kt
@@ -2,10 +2,16 @@ package com.pk4us.jetpackcompose_componentsexample.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColorScheme(
     primary = Green80,
@@ -74,6 +80,19 @@ fun Material3AppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
         darkTheme -> DarkColorPalette
         else -> LightColorPalette
     }
+
+    //__________________________________________________________________________________ скрывает элементы управления
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        // предназначены для настройки видимости панели навигации
+        systemUiController.isNavigationBarVisible = false
+
+        // предназначены для настройки видимости строки состояния соответственно
+        systemUiController.isStatusBarVisible = false
+
+        // isSystemBarsVisible для обеих системных панелей.
+        //systemUiController.isSystemBarsVisible = false
+    }//__________________________________________________________________________________
 
     MaterialTheme(
         colorScheme = colors,
